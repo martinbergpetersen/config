db8653f1ed0762e3265e1b420871876775c5252c
Martin Berg Petersen <martinbergpetersen@gmail.com>
2018-10-28 17:42:00 +0100

    Updated home settings

--- home.sh
+++ home.sh
@@ -1,1 +1,1 @@
-xrandr --auto && xrandr --output eDP1 --off && xrandr --output DP1 --right-of HDMI2 && xrandr --output HDMI2 --mode 2560x1440
+xrandr --auto && xrandr --output eDP1 --off && xrandr --output HDMI2 --right-of DP1 && xrandr --output DP1 --mode 2560x1440
