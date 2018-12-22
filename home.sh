664615c7d962e273f07195c9521168b3951af7b6
Martin Berg Petersen <martinbergpetersen@gmail.com>
2018-12-22 13:53:35 +0100

    updated home

--- home.sh
+++ home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --off --output DP-2 --off
+xrandr --output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output eDP-1 --off --output DP-2 --off
