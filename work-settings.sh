d913c2067020bc7a91c89a16839a7bf6793e77f7
Martin Berg Petersen <martinbergpetersen@gmail.com>
2019-09-16 15:53:51 +0200

    updated

--- work-settings.sh
+++ work-settings.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-xrandr --output HDMI-1 --off --output DP-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output HDMI-1 --off --output DP-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2 --right-of DP-1 --mode 2560x1440 --pos 0x0 --rotate normal
