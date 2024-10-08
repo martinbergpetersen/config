2438fade513a67334bf60ac027fe66b97e228bfe
Martin Berg Petersen <martinbergpetersen@gmail.com>
2024-10-08 20:29:55 +0200

    test

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,29 @@
+Set-PSReadLineOption -EditMode Vi -ViModeIndicator Cursor
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Set-Alias -Name cd -Value z -Option AllScope
 Set-Alias -Name cdi -Value zi -Option AllScope
+
+Set-PSReadLineKeyHandler -Chord Tab -Function Complete
+Set-PSReadLineKeyHandler -Chord "ctrl+w" -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Chord "ctrl+l" -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord "ctrl+e" -Function AcceptLine
+Set-PSReadlineKeyHandler -Key "ctrl+k" -Function  HistorySearchForward
+Set-PSReadlineKeyHandler -Key "ctrl+j" -Function  HistorySearchBackward
+
+Set-PSReadLineKeyHandler -Chord 'j' -ScriptBlock {
+  if ([Microsoft.PowerShell.PSConsoleReadLine]::InViInsertMode()) {
+    $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    if ($key.Character -eq 'k') {
+      [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+    }
+    else {
+      [Microsoft.Powershell.PSConsoleReadLine]::Insert('j')
+      [Microsoft.Powershell.PSConsoleReadLine]::Insert($key.Character)
+    }
+  }
+}
+
+
+oh-my-posh init pwsh --config C:\Users\bergp\Documents\PowerShell/tiwahu.omp.json | Invoke-Expression
+import-Module posh-git
